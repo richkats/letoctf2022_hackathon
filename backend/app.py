507e75c3a989f6b1b8c1b5705392c0100f59219a
richkats <katsur.serg@gmail.com>
2022-08-07 03:31:27 +0500

    Edit app.py

--- backend/app.py
+++ backend/app.py
@@ -11,22 +11,27 @@ def index():
     return "<h1>Sample index page</h1>"
 
 
+@app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
 
+@app.route("/list_page")
 def list_page():
     return render_template("list_page.html")
 
 
+@app.route("/statistic_page")
 def statistic_page():
     return render_template("statistic_page.html")
 
 
+@app.route("/group_page")
 def group_page():
     return render_template("group_page.html")
 
 
+@app.route("/pattern_page")
 def pattern_page():
     return render_template("pattern_page.html")
 
