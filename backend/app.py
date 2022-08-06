15ed9f042644b4175bfe8db309f8fc8dcbb287ee
richkats <katsur.serg@gmail.com>
2022-08-06 16:22:01 +0500

    Initial

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "<h1>Sample index page</h1>"
+
+
+def dashboard():
+    return "<h1>dashboard</h1>"
+
+
+def templates():
+    return "<h1>templates</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
