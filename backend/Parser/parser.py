c311fb4ec14504a8cd16e606a4f8d97492689fc1
mrixxf <mrixxf@gmail.com>
2022-08-07 08:17:30 +0300

    task ids are strings now!

---
+++ backend/Parser/parser.py
@@ -0,0 +1,13 @@
+from RSS_Creator import Main_Parser_Func, File_Lists_Path, Token_Path
+from Error_Logger import Error_Writter_txt
+from Token_Getter import Get
+
+def main():
+    try:
+        Main_Parser_Func()
+    except Exception as e:
+        print(Error_Writter_txt(e, File_Lists_Path + "Errors_Main_Parser_Func.txt"))
+    return Get(Token_Path)
+
+if __name__ == "__main__":
+    print(main())
